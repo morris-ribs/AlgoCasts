477384c18e0239ea975bd74aefefbe416ed586f3
morris-ribs <maumribeiro@gmail.com>
2018-10-16 15:21:13 +0200

    memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,8 +8,15 @@
 // Example:
 //   fib(4) === 3
 function memoize(fn) {
-    return function() {
-        
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
     }
 }
 
