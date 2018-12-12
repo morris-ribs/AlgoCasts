28e87586b03e7bb33f5df037c380bafc6cb5f373
morris-ribs <maumribeiro@gmail.com>
2018-12-12 11:45:02 +0100

    Question 7 revisited

--- exercises/messagedecoder/index.js
+++ exercises/messagedecoder/index.js
@@ -1,27 +1,30 @@
-function count(message, index = 0, counter = 1) {
+function count(message, index = 0) {
     const length = message.length;
     if (index === length) {
-        return counter;
+        return 1;
     }
 
     const num = parseInt(message[index]);
     if (num === 0) {
         return 0;
     }
-        
+
+    let counter = 0;
+
     if (num === 1) {
-        if (index < (length -1)) {
-            counter = count(message, index + 2, counter) + 1;
+        const num2 = (index < (length -1)) ? parseInt(message[index+1]) : -1;
+        if (num2 >= 0) {
+            counter = count(message, index + 2);
         }
     } else if (num === 2) {
         // num is equal to 2
-        const num2 = (index < (length -1)) ? parseInt(message[index+1]) : -1;
-        if (num2 > 0 && num2 < 7) {
-            counter = count(message, index + 2, counter) + 1;
+        const num2 = (index < (length -1)) ? parseInt(message[index+1]) : 9999;
+        if (num2 < 7) {
+            counter = count(message, index + 2);
         }
-    }
+    } 
     
-    counter = count(message, ++index, counter);
+    counter += count(message, ++index);
     return counter;
 }
 
@@ -33,6 +36,11 @@ console.log(messagedecoder("011")); // expected 0
 console.log(messagedecoder("602")); // expected 0
 console.log(messagedecoder("1602")); // expected 0
 console.log(messagedecoder("1111111602")); // expected 0
+console.log(messagedecoder("10")); // expected 1
+console.log(messagedecoder("20")); // expected 1
+console.log(messagedecoder("201")); // expected 1
+console.log(messagedecoder("106")); // expected 1
+console.log(messagedecoder("2010")); // expected 1
 console.log(messagedecoder("63945")); // expected 1
 console.log(messagedecoder("639451")); // expected 1
 console.log(messagedecoder("16945")); // expected 2
