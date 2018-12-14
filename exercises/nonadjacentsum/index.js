ff169485b3ff60e1123ca80b5b618000d28ddc5b
morris-ribs <maumribeiro@gmail.com>
2018-12-14 16:33:03 +0100

    fixes question 9

--- exercises/nonadjacentsum/index.js
+++ exercises/nonadjacentsum/index.js
@@ -1,57 +1,22 @@
-function getsum(arr, ind, length) {
-    const result = {};
-    let aux1 = ((ind >= (length - 2)) || (arr[ind + 2] < 0)) ? 0 : arr[ind + 2];
-    let aux2 = ((ind >= (length - 4)) || (arr[ind + 4] < 0)) ? 0 : arr[ind + 4];
-    let sumAux1 = aux1 + aux2;
-
-    let aux3 = ((ind >= (length - 3)) || (arr[ind + 3] < 0)) ? 0 : arr[ind + 3];
-    let aux4 = ((ind >= (length - 5)) || (arr[ind + 5] < 0)) ? 0 : arr[ind + 5];
-    let sumAux2 = aux3 + aux4;
-
-    if (sumAux1 >= sumAux2) {
-        result.sum = sumAux1;
-        if (aux2 > 0) {
-            result.newInd = ind + 4;
-        } else {
-            result.newInd = ind + 2;
-        }
-    } else {
-        result.sum = sumAux2;
-        if (aux4 > 0) {
-            result.newInd = ind + 5;
-        } else {
-            result.newInd = ind + 3;
-        }
-    }
-    return result;
-}
-
 function nonadjacentsum(arr) {
-    // retrieve the first positive number in the array
-    let ind1 = arr.findIndex(num => num > 0);
-    
-    // there are no positive numbers, return the max
-    if (ind1 < 0) {
-        return Math.max(...arr);
-    }
-
-    let ind2 = ind1 + 1;
-    let sum1 = arr[ind1];
-    let sum2 = (arr[ind2] >= 0) ? arr[ind2] : 0;
+    const max = Math.max(...arr);
 
     const length = arr.length;
-
-    while(ind1 < length && ind2 < length) {
-        let sumObj = getsum(arr, ind1, length);
-        sum1 += sumObj.sum;
-        ind1 = sumObj.newInd;
-
-        sumObj = getsum(arr, ind2, length);
-        sum2 += sumObj.sum;
-        ind2 = sumObj.newInd;
+    if (length <= 2 || max <= 0) {
+        return max;
     }
 
-    return (sum1 >= sum2) ? sum1 : sum2;
+    let maxExcludingLast = Math.max(0, arr[0]);    
+    let maxIncludingLast = Math.max(maxExcludingLast, arr[1]);
+    arr.splice(0, 2);
+    arr.forEach(element => {
+        const prevMaxIncludingLast = maxIncludingLast;
+
+        maxIncludingLast = Math.max(maxIncludingLast, maxExcludingLast + element);
+        maxExcludingLast = prevMaxIncludingLast;
+    });
+
+    return Math.max(maxIncludingLast, maxExcludingLast);
 }
 
 console.log(nonadjacentsum([2,4,6,2,5]));
@@ -65,6 +30,6 @@ console.log(nonadjacentsum([-2,-4,6,-2,-5]));
 console.log(nonadjacentsum([-5,-1,1,-5]));
 console.log(nonadjacentsum([-2,-4,6,-2,5]));
 console.log(nonadjacentsum([-2,-4,6,2,-5]));
-console.log(nonadjacentsum([0,0,0,0,0,0,0,55]));
+console.log(nonadjacentsum([12,10,2,7,5,1,55,1]));
 
 module.exports = nonadjacentsum;
