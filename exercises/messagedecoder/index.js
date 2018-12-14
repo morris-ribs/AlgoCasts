dad437f4e3d64ca7049f6c7e064706d273fa920d
morris-ribs <maumribeiro@gmail.com>
2018-12-14 12:07:26 +0100

    Coding Question 9

--- exercises/messagedecoder/index.js
+++ exercises/messagedecoder/index.js
@@ -1,4 +1,17 @@
-function count(message, index = 0) {
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+
+function slowCount(message, index = 0) {
     const length = message.length;
     if (index === length) {
         return 1;
@@ -27,6 +40,8 @@ function count(message, index = 0) {
     counter += count(message, ++index);
     return counter;
 }
+
+const count = memoize(slowCount);
 
 function messagedecoder(message) {
     return count(message);
