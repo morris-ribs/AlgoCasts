8bd131b85379bbaf3efa548821c397aa1d992ddb
morris-ribs <maumribeiro@gmail.com>
2018-12-16 12:27:38 +0100

    Trees start

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -42,7 +42,12 @@ class Tree {
     }
 
     traverseDF(fn) {
-
+        const arr = [this.root];
+        while(arr.length) {
+            const node = arr.shift();
+            arr.unshift(...node.children);
+            fn(node);
+        }
     }
 }
 
