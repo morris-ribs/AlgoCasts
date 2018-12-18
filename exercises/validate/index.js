faacd5e76dad950d4f348147673ccd7d2c2a0f5c
morris-ribs <maumribeiro@gmail.com>
2018-12-18 15:09:06 +0100

    Validate BST'

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -4,7 +4,32 @@
 // less than the parent node's value, and that
 // every node's right hand child is greater than
 // the parent
+function validate(node, min = null, max = null) {
+    if (node == null) {
+        return true;
+    }
 
-function validate(node, min = null, max = null) {}
+    if (min != null && node.data < min) {
+        return false;
+    }
+
+    if (max != null && node.data > max) {
+        return false;
+    }
+
+    let isLeftOk = true, isRightOk = true;
+
+    // left hand
+    if (node.left) {
+        isLeftOk = validate(node.left, min, node.data);
+    }
+
+    // right hand
+    if (node.right) {
+        isRightOk = validate(node.right, node.data, max);
+    }
+
+    return isLeftOk && isRightOk;
+}
 
 module.exports = validate;
